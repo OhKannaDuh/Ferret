f34c2fc7aa432430d12be75dab909f3ed9e6299e
OhKannaDuh <uimokd@gmail.com>
2025-05-09 20:04:49 +0100

    Added a base model class

---
+++ lib/Ferret/Model.lua
@@ -0,0 +1,10 @@
+--------------------------------------------------------------------------------
+--   DESCRIPTION: Base model class
+--        AUTHOR: Faye (OhKannaDuh)
+--------------------------------------------------------------------------------
+
+---@class Model : Object
+Model = Object:extend()
+Model:implement(Translation)
+
+return Model
